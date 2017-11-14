c2d31878d47006d9dc44de6bb384248c8f8760b2
mauridb <mauriziocontatto@gmail.com>
2017-11-14 22:43:28 +0100

    UPDATE: added modal home page sign up with confirm password and info logs

--- authC/views.py
+++ authC/views.py
@@ -11,19 +11,33 @@ from authC.models import User, Project
 
 def singUp(request):
     if request.method == 'POST':
+
         user_id = User.objects.all().count() + 1  # TODO: better logic user.last.user_id + 1
-        prj1 = Project(name=request.POST.get("prjname", None))
+        # prj1 = Project(name=request.POST.get("prjname", None))
         first_name = request.POST.get("first_name", None)
         last_name = request.POST.get("last_name", None)
-        password = request.POST.get("pwd", None)
-        email = request.POST.get("email", None)
-        role = request.POST.get("role", None)
+        password = request.POST.get("password", None)
+        confirm_password = request.POST.get("confirm_password", None)
+        email = request.POST.get("sign_email", None)
+        role = request.POST.get("role", 'user')
+
+        if password != confirm_password:
+            return HttpResponse('different password')
+
+        # Logs sign up
+        print c('USER REGISTERED INFO:', 'blue')
+        print c('\tUSER FIRST NAME: %s' % first_name, 'green')
+        print c('\tUSER LAST NAME: %s' % last_name, 'green')
+        print c('\tUSER EMAIL: %s' % email, 'green')
+        print c('\tUSER ROLE: %s' % role, 'green')
+        print c('\tUSER PASSWORD: ******', 'green')
+
         user = User(user_id=user_id, first_name=first_name, last_name=last_name, role=role, email=email,
                     password=password)
-        user.projects.append(prj1)
+        # user.projects.append(prj1)
         user.save()
         return HttpResponse("""Well Done! Check your email to confirm..
-                <a href="/authC/signUp/">test again</a>
+                <a href="/">Homepage</a>
                 """)
     else:
         return render(request, 'testcrud.html')
@@ -36,13 +50,12 @@ def signIn(request):
         email = request.POST.get("login_email", None)
         password = request.POST.get("password", None)
 
-        # Logs
+        # Logs sign in
         print c('USER LOGGED INFO:', 'red')
         print c('\tUSER FIRST NAME: %s' % first_name, 'yellow')
         print c('\tUSER LAST NAME: %s' % last_name, 'yellow')
         print c('\tUSER EMAIL: %s' % email, 'yellow')
         print c('\tUSER PASSWORD: ******', 'yellow')
-
 
         try:
             user = User.objects.get(first_name=first_name, last_name=last_name, email=email, password=password)
