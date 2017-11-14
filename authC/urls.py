f911d24cf86ffd3ae6f841e44d5e6b8ec07f54ac
mauridb <mauriziocontatto@gmail.com>
2017-11-14 21:34:34 +0100

    FIX: move sign up to authC/signUp route

--- authC/urls.py
+++ authC/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from . import views
+from authC import views
 
 urlpatterns = [
 
-
+    # Sign Up registration
+    url(r'^signUp/$', views.singUp, name='sign-up'),
 
 ]
--- authC/views.py
+++ authC/views.py
@@ -3,10 +3,28 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 
-from .models import User, Project
+from authC.models import User, Project
 
+
+def singUp(request):
+    if request.method == 'POST':
+        user_id = User.objects.all().count() + 1  # TODO: better logic user.last.user_id + 1
+        prj1 = Project(name=request.POST.get("prjname", None))
+        first_name = request.POST.get("first_name", None)
+        last_name = request.POST.get("last_name", None)
+        password = request.POST.get("pwd", None)
+        email = request.POST.get("email", None)
+        role = request.POST.get("role", None)
+        user = User(user_id=user_id, first_name=first_name, last_name=last_name, role=role, email=email,
+                    password=password)
+        user.projects.append(prj1)
+        user.save()
+        return HttpResponse("""Well Done! Check your email to confirm..
+                <a href="/authC/signUp/">test again</a>
+                """)
+    else:
+        return render(request, 'testcrud.html')
 
 
 
